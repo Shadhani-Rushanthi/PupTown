6bbd836cf744383fd3f3bcabad3811995aab6d53
Shadhani Rushanthi <shadhanir@gmail.com>
2023-02-04 00:30:32 +0530

    first

---
+++ puptown/src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h3 class='d-flex justify-content-center m-2'>
+        Pup Town
+      </h3>
+    </div>
+  );
+}
+
+export default App;
